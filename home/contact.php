374c7cc89ad66fbd977218c73222a3d847957b1c
MRijk <mjrijk@hotmail.com>
2022-04-21 23:50:09 +0200

    file removals and final finishing touches

---
+++ home/contact.php
@@ -0,0 +1,98 @@
+<?php
+
+require_once('PHPMailer-6.6.0/src/PHPMailer.php');
+require_once('PHPMailer-6.6.0/src/Exception.php');
+require_once('PHPMailer-6.6.0/src/SMTP.php');
+
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+ini_set('display_errors', true);
+
+// set initial header to forbidden
+header($_SERVER["SERVER_PROTOCOL"]." 503  Forbidden"); 
+// check _POST fields
+
+$max_name_size=60;
+$max_company_size=60;
+$max_email_size=60;
+$max_body_size=2048;
+
+if (!isset($_POST["name"]) || !isset($_POST["company"]) || !isset($_POST["email"]) || !isset($_POST["body"]))
+{
+	header($_SERVER["SERVER_PROTOCOL"]." 402 Payload Required");
+	echo "Unfortunately no message was sent";	
+	exit(0);
+}
+
+if (strlen($_POST["name"]) > $max_name_size ||
+	strlen($_POST["company"]) > $max_company_size ||
+	strlen($_POST["email"]) > $max_email_size ||
+	strlen($_POST["body"]) > $max_body_size )
+{
+	header($_SERVER["SERVER_PROTOCOL"]." 413 Payload Too Large");
+	echo "inputfields contain too much data";
+	exit(0); 
+}
+
+$body_contents = "message sent by: " . $_POST["name"] . " from " . $_POST["company"] . "\r\n\r\n";
+$body_contents .= "e-mail: " . $_POST["email"] . "\r\n\r\n" . "\r\n\r\n";
+$body_contents .= "message: \r\n\r\n" . $_POST["body"];
+
+if (extension_loaded('openssl')) 
+{
+  //  echo 'openssl extension loaded.';
+}
+
+$mail = new PHPMailer(true);
+// testing code
+$mail->SMTPOptions = array(
+    'ssl' => array(
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    )
+);
+
+// end testing code
+$mail->CharSet =  "utf-8";
+$mail->IsSMTP();
+$mail->SMTPAutoTLS = false;
+$mail->SMTPAuth = true;                  
+$mail->Username='contact@myrtherijk.nl';
+$mail->Password = "4PDK%T5Wr4Skz8u";
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->SMTPOptions = [
+    'ssl' => [
+        'verify_peer' => false,
+        'verify_peer_name' => false,
+        'allow_self_signed' => true
+    ]
+    ];
+$mail->Host = "smtp.strato.com";
+
+// set the SMTP port for the GMAIL server
+$mail->Port = "587";
+// $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->From='contactform@myrtherijk.nl';
+$mail->FromName='contact form';
+$mail->AddAddress('mjrijk@hotmail.com');
+$mail->Subject  =  'contact form myrtherijk.nl by :' . $_POST['name'] . " " . $_POST['company'];
+$mail->IsHTML(true);
+$mail->Body    = $body_contents;
+// *************************** The server hangs right here ***********************
+if($mail->Send())
+{
+	header($_SERVER["SERVER_PROTOCOL"]." 200  OK"); 
+    echo "Your message has been sent, thank you!";
+}
+else 
+{
+	header($_SERVER["SERVER_PROTOCOL"]." 417 Expectation Failed"); 
+    echo "Unfortunately no message was sent";
+}
+ 
+
+?>
--- home/script-home.js
+++ home/script-home.js
@@ -156,7 +156,7 @@
             ctx.beginPath();
             ctx.moveTo(p.x, p.y);
             ctx.lineTo(p.closest[i].x, p.closest[i].y);
-            ctx.strokeStyle = 'rgba(156,217,249,'+ p.active+')';
+            ctx.strokeStyle = 'rgb(54, 2, 2,'+ p.active+')';
             ctx.stroke();
         }
     }
@@ -175,7 +175,7 @@
             if(!_this.active) return;
             ctx.beginPath();
             ctx.arc(_this.pos.x, _this.pos.y, _this.radius, 0, 2 * Math.PI, false);
-            ctx.fillStyle = 'rgba(156,217,249,'+ _this.active+')';
+            ctx.fillStyle = 'rgba(255,130,0,'+ _this.active+')';
             ctx.fill();
         };
     }
@@ -187,6 +187,3 @@
     
 })();
 
-AOS.init({
-    duration: 900
-  })
